61d01659758620f3946842eb4ccc48cc4d7c81e7
nahid <nahidcse.pub@gmail.com>
2024-01-19 15:11:07 +0600

    complete navbar

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,11 +1,27 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Link from 'next/link'
 import { Bars3CenterLeftIcon } from '@heroicons/react/24/solid'
 import { ThemeSwitcher } from '@/components/ThemeSwitcher'
 
 const Navbar = () => {
     const [toggleButton, setToggleButton] = useState(false)
+    let menuRef = useRef()
+
+    let handleClickOutside = (e) => {
+        if (!menuRef.current?.contains(e.target)) {
+            setToggleButton(false)
+        }
+    }
+
+    useEffect(() => {
+        console.log('test')
+        document.addEventListener('mousedown', handleClickOutside)
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+    })
 
     const urls = [
         { path: '/events', title: 'Events' },
@@ -14,7 +30,7 @@ const Navbar = () => {
     ]
 
     return (
-        <div className="bg-purple-800 relative">
+        <div className="bg-purple-800">
             <div
                 className="px-8 md:px-0 py-4 flex items-center justify-between text-white max-w-[1216px] w-full m-auto">
                 <div>
@@ -35,29 +51,33 @@ const Navbar = () => {
                             </li>
                         </ul>
                     </div>
-                    <button
-                        onClick={() => setToggleButton(!toggleButton)}
-                        className="md:hidden hover:bg-purple-600 p-2 rounded-2xl"
-                    >
-                        <Bars3CenterLeftIcon className="h-6 w-6" />
-                    </button>
+                    <div className='relative'>
+                        <div ref={menuRef}>
+                            <button
+                                onClick={() => setToggleButton(!toggleButton)}
+                                className="md:hidden hover:bg-purple-600 p-2 rounded-2xl"
+                            >
+                                <Bars3CenterLeftIcon className="h-6 w-6" />
+                            </button>
+                            <div
+                                className={`${toggleButton ? '' : 'hidden'} absolute -right-3 top-16 rounded-2xl bg-purple-800 w-40 p-8 text-white`}
+                            >
+                                <ul className="w-full text-center">
+                                    {urls.map((url, index) => (
+                                        <li key={index} className="py-2">
+                                            <Link href={url.path} onClick={() => setToggleButton(false)}>
+                                                {url.title}
+                                            </Link>
+                                        </li>
+                                    ))}
+                                    <li className="py-2">
+                                        <ThemeSwitcher />
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div
-                className={`${toggleButton ? '' : 'hidden'} absolute bg-purple-800 w-full border-t border-black p-8 text-white`}
-            >
-                <ul className="w-full text-center">
-                    {urls.map((url, index) => (
-                        <li key={index} className="py-2">
-                            <Link href={url.path} onClick={() => setToggleButton(false)}>
-                                {url.title}
-                            </Link>
-                        </li>
-                    ))}
-                    <li className="py-2">
-                        <ThemeSwitcher />
-                    </li>
-                </ul>
             </div>
         </div>
     )
