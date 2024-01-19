256c25bb8367f694401df90df87f21c07f0f3c8e
nahid <nahidcse.pub@gmail.com>
2024-01-19 15:11:07 +0600

    complete navbar

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,21 +1,25 @@
-import {Inter} from 'next/font/google'
+import { Inter } from 'next/font/google'
 import './globals.css'
-import {ThemeProvider} from "./providers";
+import { ThemeProvider } from './providers'
+import Navbar from '@/components/Navbar'
 
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
     title: 'Vote App',
     description: 'Created by Nahid Hasan',
 }
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
     return (
         <html lang="en" suppressHydrationWarning={true}>
-        <body className={`${inter.className} bg-slate-50 dark:bg-[#0d1117]`}>
+        <body className={`${inter.className} bg-slate-50 dark:bg-[#0d1117] dark:text-white`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            {children}
+            <Navbar />
+            <div className="max-w-[1216px] w-full m-auto px-4 md:px-0">
+                {children}
+            </div>
         </ThemeProvider>
         </body>
         </html>
--- client/app/page.js
+++ client/app/page.js
@@ -1,9 +1,10 @@
 import Navbar from '@/components/Navbar'
+import Login from '@/app/login'
 
 export default function Home() {
   return (
     <div>
-        <Navbar/>
+      <Login/>
     </div>
   )
 }
