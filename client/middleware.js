b684045f427a92ad87936703563c86c50168205e
nahid-git <nahidcse.pub@gmail.com>
2024-01-29 03:36:57 +0600

    set private route

--- client/middleware.js
+++ client/middleware.js
@@ -1,19 +1,25 @@
 import { NextResponse } from 'next/server'
 
 export function middleware(request) {
-    // const authenticate = request.cookies.get('next-auth.session-token')?.value
-    //
-    // if (authenticate && request.nextUrl.pathname === '/login') {
-    //     return NextResponse.redirect(new URL('/profile', request.url))
-    // }
-    // if (authenticate && request.nextUrl.pathname === '/signup') {
-    //     return NextResponse.redirect(new URL('/profile', request.url))
-    // }
-    // if (!authenticate && request.nextUrl.pathname === '/profile') {
-    //     return NextResponse.redirect(new URL('/login', request.url))
-    // }
+    const authenticate = request.cookies.get('next-auth.session-token')?.value
+
+    if (authenticate && request.nextUrl.pathname === '/auth/login') {
+        return NextResponse.redirect(new URL('/profile', request.url))
+    }
+    if (authenticate && request.nextUrl.pathname === '/auth/signup') {
+        return NextResponse.redirect(new URL('/profile', request.url))
+    }
+    if (authenticate && request.nextUrl.pathname === '/auth/forgot_password') {
+        return NextResponse.redirect(new URL('/profile', request.url))
+    }
+    if (authenticate && request.nextUrl.pathname === '/auth/reset_password') {
+        return NextResponse.redirect(new URL('/profile', request.url))
+    }
+    if (!authenticate && request.nextUrl.pathname === '/profile') {
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
 }
 
-export const config = {
-    matcher: ['/profile'],
-}
+// export const config = {
+//     matcher: ['/profile'],
+// }
