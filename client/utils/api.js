53ad6db9d4f901010eee1e40d48d36a9cdccca0d
nahid-git <nahidcse.pub@gmail.com>
2024-01-30 00:13:38 +0600

    change folder

--- client/utils/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-import process from 'next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss'
-
-export const httpClient = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL_SERVER,
-    headers: {
-        'Content-Type': 'application/json',
-    },
-})
